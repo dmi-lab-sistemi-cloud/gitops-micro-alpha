4e804264f995f938e145b658b19a3341c1f9c5fe
Andrea Fornaia <57360234+afornaia@users.noreply.github.com>
2022-06-17 17:49:36 +0200

    Fix

--- src/main/java/com/example/microalpha/MicroAlphaController.java
+++ src/main/java/com/example/microalpha/MicroAlphaController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MicroAlphaController 
+public class MicroAlphaController {
 	@GetMapping("/")
 	public String hello() {
 		return "<h1>UPDATED Hello from MicroApha!</h1>\n";
